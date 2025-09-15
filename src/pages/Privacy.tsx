285345e1c7c9893124b850e1c451afb8aaf3472a
Rishi Swami <rishiswami77@outlook.com>
2025-09-15 20:54:44 +0530

    privacy terms Edits

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -3,7 +3,7 @@ import logos from "../components/HeadSection/logo.png"
 const Privacy = () => {
   return (
     <div className="text-light">
-      <div style={{ margin: "0 270px" }}>
+      <div className="respon">
         <div className="row d-flex justify-content-center text-center text-light">
           <div className="col-12">
             <p className="text-primary">Privacy Policy</p>
@@ -14,12 +14,11 @@ const Privacy = () => {
           <div className="col">
             <p>Your privacy is important to us at videoTube. We repect your privacy regarding any information we may collect from you across out website.</p>
           </div>
+          <hr className="hrliness" />
         </div>
-
-
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate facilis officia magni quidem labore obcaecati sequi repellat sit iusto modi doloribus excepturi consectetur, numquam quis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo voluptatum provident eveniet neque impedit ipsa molestias, animi reiciendis dolorum vitae cumque, itaque quae. Repudiandae obcaecati eaque perferendis fuga amet at?</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad excepturi optio consequuntur quas deleniti sequi laboriosam rem, necessitatibus voluptates modi commodi exercitationem eos expedita repellendus? Quisquam reiciendis illo deleniti, eligendi architecto velit veniam. Odit repudiandae rem, recusandae placeat suscipit non incidunt similique optio facilis quae architecto minus iusto modi natus quod quidem laboriosam unde error explicabo ullam! Enim unde iste, beatae, corrupti, velit animi perferendis nisi maxime dolore qui aliquam.</p>
-        <h3>What information do we collect?</h3>
+        <h4>What information do we collect?</h4>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit tenetur aperiam excepturi deleniti, tempora quidem magni obcaecati quas. Quo perspiciatis, deserunt velit porro ipsum, quod sit ratione cupiditate cumque aperiam dolorem adipisci architecto dolor id quisquam aliquid aspernatur. Repellat, consectetur? At harum necessitatibus nisi rem ullam sapiente laborum eaque? Harum?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro at iusto laboriosam possimus in! Repellendus ea accusantium omnis impedit incidunt facere fuga consectetur quidem sint dolorem necessitatibus corporis itaque sed qui voluptates amet, sunt fugit? Deleniti explicabo et veniam alias eligendi similique, molestiae ipsa vitae rerum non repudiandae accusamus error temporibus pariatur. Sit totam, odit facilis adipisci ut vitae neque deserunt obcaecati. Laborum labore possimus ipsa quo placeat, tempora voluptatibus commodi itaque quia cupiditate nihil at, porro deleniti totam quaerat!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum fuga asperiores voluptatibus quidem dolores, quos officiis nostrum veritatis. A praesentium dicta temporibus vitae, excepturi sequi itaque? Pariatur veniam praesentium error qui necessitatibus. Et autem dolorum consectetur officia, provident quasi soluta?</p>
--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -2,7 +2,7 @@ import logos from "../components/HeadSection/logo.png"
 const Terms = () => {
   return (
     <div className="text-light">
-      <div style={{ margin: "0 270px" }}>
+      <div className="respon">
         <div className="row d-flex justify-content-center text-center text-light">
           <div className="col-12">
             <p className="text-primary">Current as of 20 jan 2022</p>
@@ -13,13 +13,13 @@ const Terms = () => {
           <div className="col">
             <p>By accessing our website, you are agreeing to be bound by these terms of service, and agree that you are responsible for compliance with any applicable local laws.</p>
           </div>
-          <hr className="border" />
+          <hr className="hrliness" />
         </div>
 
 
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate facilis officia magni quidem labore obcaecati sequi repellat sit iusto modi doloribus excepturi consectetur, numquam quis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo voluptatum provident eveniet neque impedit ipsa molestias, animi reiciendis dolorum vitae cumque, itaque quae. Repudiandae obcaecati eaque perferendis fuga amet at?</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad excepturi optio consequuntur quas deleniti sequi laboriosam rem, necessitatibus voluptates modi commodi exercitationem eos expedita repellendus? Quisquam reiciendis illo deleniti, eligendi architecto velit veniam. Odit repudiandae rem, recusandae placeat suscipit non incidunt similique optio facilis quae architecto minus iusto modi natus quod quidem laboriosam unde error explicabo ullam! Enim unde iste, beatae, corrupti, velit animi perferendis nisi maxime dolore qui aliquam.</p>
-        <h3>What information do we collect?</h3>
+        <h4>What information do we collect?</h4>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit tenetur aperiam excepturi deleniti, tempora quidem magni obcaecati quas. Quo perspiciatis, deserunt velit porro ipsum, quod sit ratione cupiditate cumque aperiam dolorem adipisci architecto dolor id quisquam aliquid aspernatur. Repellat, consectetur? At harum necessitatibus nisi rem ullam sapiente laborum eaque? Harum?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro at iusto laboriosam possimus in! Repellendus ea accusantium omnis impedit incidunt facere fuga consectetur quidem sint dolorem necessitatibus corporis itaque sed qui voluptates amet, sunt fugit? Deleniti explicabo et veniam alias eligendi similique, molestiae ipsa vitae rerum non repudiandae accusamus error temporibus pariatur. Sit totam, odit facilis adipisci ut vitae neque deserunt obcaecati. Laborum labore possimus ipsa quo placeat, tempora voluptatibus commodi itaque quia cupiditate nihil at, porro deleniti totam quaerat!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum fuga asperiores voluptatibus quidem dolores, quos officiis nostrum veritatis. A praesentium dicta temporibus vitae, excepturi sequi itaque? Pariatur veniam praesentium error qui necessitatibus. Et autem dolorum consectetur officia, provident quasi soluta?</p>
