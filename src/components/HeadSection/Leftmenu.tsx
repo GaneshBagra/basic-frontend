087e7c6927eb364ea628cabfa845a2cf37365e77
Ganesh Bagra <ganeshbagra003@gmail.com>
2025-09-15 18:48:51 +0530

    Update Leftmenu navigation and add routes for video sections

--- src/components/HeadSection/Leftmenu.tsx
+++ src/components/HeadSection/Leftmenu.tsx
@@ -1,45 +1,47 @@
 import { AiOutlineLike } from "react-icons/ai"
 import { CiCircleQuestion, CiSettings } from "react-icons/ci"
 import { FaFolder, FaHistory, FaHome, FaUserCheck, FaVideo } from "react-icons/fa"
+import { useNavigate } from "react-router-dom"
 
 const Leftmenu = () => {
+    const navigate = useNavigate();
     return (
         <div className="main_box">
 
             <div className="sidebarl">
                 <div className="menul m-10">
                     <ul className="list-unstyled">
-                        <li>
+                        <li onClick={() => navigate(`/home`)} >
                             <FaHome className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">Home</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">Home</a>
                         </li>
-                        <li>
+                        <li onClick={() => navigate(`/liked-videos`)}>
                             <AiOutlineLike className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">Liked Videos</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">Liked Videos</a>
                         </li>
-                        <li>
+                        <li onClick={() => navigate(`/history`)}>
                             <FaHistory className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">History</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">History</a>
                         </li>
-                        <li>
+                        <li onClick={() => navigate(`/my-content`)}>
                             <FaVideo className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">My Content</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">My Content</a>
                         </li>
-                        <li>
+                        <li onClick={() => navigate(`/collections`)}>
                             <FaFolder className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">Collections</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">Collections</a>
                         </li>
                         <li>
                             <FaUserCheck className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">Subscribers</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">Subscribers</a>
                         </li>
                         <li className="supportl">
                             <CiCircleQuestion className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">Supports</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">Supports</a>
                         </li>
                         <li className="settingsl">
                             <CiSettings className="text-light m-2" />
-                            <a href="#" className="link-underline text-light link-underline-opacity-0">Setting</a>
+                            <a  className="link-underline text-light link-underline-opacity-0">Setting</a>
                         </li>
                     </ul>
                 </div>
