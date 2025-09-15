2354c797283301ebf3822b951275fe899685b129
Rishi Swami <rishiswami77@outlook.com>
2025-09-15 20:25:49 +0530

    privacy

--- src/components/layout/privacyPageLayout.tsx
+++ src/components/layout/privacyPageLayout.tsx
@@ -4,7 +4,7 @@ import PrivacyHeader from '../privacy&T&CHeader/privacyHeader';
 const PrivacyPageLayout = () => (
     <>
         <PrivacyHeader />
-        <main className="content-for-privacy-and-T&C" style={{height:"100vh",width:"100vw"}}>
+        <main className="content-for-privacy-and-T&C" style={{height:"calc(100% - 80px)",width:"100%"}}>
             <Outlet />
         </main>
     </>
