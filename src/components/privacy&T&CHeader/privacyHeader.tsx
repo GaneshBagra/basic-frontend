f83d847c1d91f73908821702b9d20a5ee65160a9
Rishi Swami <rishiswami77@outlook.com>
2025-09-15 20:21:58 +0530

    add terms and privacy pages

--- src/components/privacy&T&CHeader/privacyHeader.tsx
+++ src/components/privacy&T&CHeader/privacyHeader.tsx
@@ -25,33 +25,34 @@ const PrivacyHeader = () => {
                 <button className='btns2'>Sign up</button>
 
             </div>
-             <div>
-            <input type="checkbox" className="inputhider" id="click" />
-            <div className="barp" id="click">
-                <label htmlFor="click" className='text-light mt-1'><IoMdMenu /></label>
-            </div>
-            <div className="sidebarp p-2 h-100 text-light">
-                <div className="menup m-10">
-                    <img className='imges' src={logos} alt="this is not avlible" />
-                    <ul className='list-unstyled'>
-                        <li>
-                            <a href="#"  className="link-underline text-light link-underline-opacity-0">Home</a>
-                        </li>
-                        <li>
-                            <a href="#"  className="link-underline text-light link-underline-opacity-0">Products</a>
-                        </li>
-                        <li>
-                            <a href="#"  className="link-underline text-light link-underline-opacity-0">Resources</a>
-                        </li>
-                        <li>
-                            <a href="#"  className="link-underline text-light link-underline-opacity-0">Pricing</a>
-                        </li>
-                    </ul>
-                    <button className='btns1-menup'>Log in</button>
-                    <button className='btns2-menup'>Sign up</button>
+            <hr className='hrline' />
+            <div className='sidep'>
+                <input type="checkbox" className="inputhider" id="click" />
+                <div className="barp" id="click">
+                    <label htmlFor="click" className='text-light mt-1'><IoMdMenu /></label>
+                </div>
+                <div className="sidebarp p-2 h-100 text-light">
+                    <div className="menup m-10">
+                        <img className='imges' src={logos} alt="this is not avlible" />
+                        <ul className='list-unstyled'>
+                            <li>
+                                <a href="#" className="link-underline text-light link-underline-opacity-0">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" className="link-underline text-light link-underline-opacity-0">Products</a>
+                            </li>
+                            <li>
+                                <a href="#" className="link-underline text-light link-underline-opacity-0">Resources</a>
+                            </li>
+                            <li>
+                                <a href="#" className="link-underline text-light link-underline-opacity-0">Pricing</a>
+                            </li>
+                        </ul>
+                        <button className='btns1-menup'>Log in</button>
+                        <button className='btns2-menup'>Sign up</button>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
 
     )
