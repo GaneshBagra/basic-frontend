fa8bb360bffcacf6722ea999e9fe6daa170a35d4
Ganesh Bagra <ganeshbagra003@gmail.com>
2025-09-15 14:22:02 +0530

    Some changes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import { } from 'react-bootstrap';
 import logos from './logo.png'
 import type React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 interface rnderProps{
     onClick:()=>void;
 }
@@ -11,7 +12,9 @@ const {onClick} = props;
     return (
         <div className='mens'>
             <img className='mensimg' src={logos} />
-            <button className='btns4' onClick={onClick}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg></button>
+            <button className='btns4' onClick={onClick}>
+                <GiHamburgerMenu />
+            </button>
  
         </div>
     )
